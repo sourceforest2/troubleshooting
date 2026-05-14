0fcc428bc13f5d91d6ae99c751c6eb2bceb036d5
R.Ouali <sourceforest@gmail.com>
2026-05-14 10:37:51 +0100

    ajouter cli utils

---
+++ utils_cli.sh
@@ -0,0 +1,7 @@
+
+
+# Pour récupérer les logs système dans la plage temporelle — très pratique et fiable.
+journalctl --since "$DATE_DEBUT" --until "$DATE_FIN"
+
+
+# tfactl analyze ou grep selon disponibilité pour Oracle dans rap_bdd ?
